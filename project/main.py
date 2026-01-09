9093aebcfdcb454bb79a8ddc19c94b2239b7e1ca
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-01-09 13:13:54 +0100

    Add a path parameter

--- project/main.py
+++ project/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# Root endpoint
 @app.get('/')
 def root():
     return {"message":"Hello FastApI"}
+
+# Path parameter
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    return {"user_id":user_id}
