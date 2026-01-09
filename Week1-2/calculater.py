d0057e54da17b39d51004c746f69899d6fac5b4d
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-01-09 15:32:24 +0100

    Display formatted results to the user

--- Week1-2/calculater.py
+++ Week1-2/calculater.py
@@ -6,3 +6,16 @@ revenue =  float(input("Enter total revenue`; $"))
 # Get costs from user
 costs = float(input("Enter total consts: $"))
 
+# Calculate profit
+profit = revenue - costs
+
+# Caclulate profit margin percentage
+margin = (profit / revenue) * 100
+
+# Display results
+
+print("\n--- Financial Summary ---")
+print(f"Revenue: ${revenue:}")
+print(f"Costs: ${costs}")
+print(f"Profit: ${profit}")
+print(f"Profit Margin: ${margin}")
