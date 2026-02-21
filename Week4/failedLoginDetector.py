6030aae47a354f6a740c6fe9838f9405f01daa3d
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-02-21 13:57:48 +0100

    Count failed attempts for each user and alert if 3+ attempts seen

--- Week4/failedLoginDetector.py
+++ Week4/failedLoginDetector.py
@@ -6,3 +6,20 @@ login_attempts = [
 ("Ahmad", "failed"),
 ("Elyas", "failed")
 ]
+
+
+failed_counts = {}
+# count failed login attempts
+for username, status in login_attempts:
+    if status =='failed':
+        if username in failed_counts:
+            failed_counts[username] = failed_counts[username] + 1
+        else:
+            failed_counts[username] = 1
+
+# Check for accounts that failed 3 or more times
+for username in failed_counts:
+    if failed_counts[username]>= 3:
+        print("ALERT: User '" + username + "' has " + str(failed_counts[username]) + " failed login attempts")
+
+print('Security check completed')
