17a25473ffdbc276b9a9b35cf8647e31de736b76
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-02-25 12:07:56 +0100

    Test the library system

--- Week 9/librarySystem.py
+++ Week 9/librarySystem.py
@@ -30,11 +30,11 @@ class Library:
     def remove_book(self, isbn: str):
         # Remove a book by ISBN
         for book in self.books:
-            if self.book.isbn == isbn:
+            if book.isbn == isbn:
                 self.books.remove(book)
-                print(f'Book "{book.title}" remove from the library.')
+                print(f'Book "{book.title}" removed from the library.')
                 return
-            print("Book not found. No removal peroformed.")
+        print("Book not found. No removal performed.")
 
     def list_books(self):
         # Display all books in the library
@@ -51,8 +51,37 @@ class Library:
         found = False
         for book in self.books:
             if title.lower() in book.title.lower():
-                print(book.dispalay_info())
+                print(book.display_info())
                 found = True
 
             if not found:
                 print("No matching books found.")
+
+# --------------------------------------------------
+# Testing the Library System
+# --------------------------------------------------
+
+# Create a library instance
+library = Library()
+
+# Add books to the libarary
+book1 = Book("Atommic Habits", "James Clear", "ISBN001")
+book2 = Book("The art of Social Engineering","Hadnagy", "ISBN002")
+book3 = Book("The Alchemist", "Paulo Coelho","ISBN003")
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+# List all books
+library.list_books()
+
+# Search for a book by title
+library.search_by_title("The Alchemist")
+library.search_by_title("ATOMIC HABITS")
+
+# Remove a book and verify removal
+library.remove_book('ISBN001')
+
+# List books again to confirm removal
+library.list_books()
