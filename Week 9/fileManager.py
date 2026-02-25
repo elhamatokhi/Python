d2ac0238bfc43483081c50491f4dc77a1d1112de
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-02-25 13:51:44 +0100

    Print current working directory

--- Week 9/fileManager.py
+++ Week 9/fileManager.py
@@ -3,3 +3,7 @@ import os
 """
 File System Operations Script - file handling using the os module
 """
+
+# Display the current working directory
+current_dir = os.getcwd()
+print(f"Current working directory: {current_dir}")
